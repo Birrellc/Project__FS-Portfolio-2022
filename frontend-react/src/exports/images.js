670e8b84c1fbe337850fd5f1b6d70e94c0240df3
Chris <65775571+Birrellc@users.noreply.github.com>
2022-05-15 19:07:23 +0100

    Feat: Setup frontend file structure

---
+++ frontend-react/src/exports/images.js
@@ -0,0 +1,27 @@
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import hero from '../assets/hero.png';
+
+export default {
+  mobile,
+  api,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  python,
+  react,
+  redux,
+  sass,
+  hero,
+};
