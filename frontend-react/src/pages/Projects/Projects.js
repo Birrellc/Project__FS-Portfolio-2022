8eb07a498dd2a89f335ed17bd8d368acf7006e76
Chris <65775571+Birrellc@users.noreply.github.com>
2022-05-16 21:20:02 +0100

    Fix: corrected works code after renaming to projects

--- frontend-react/src/pages/Projects/Projects.js
+++ frontend-react/src/pages/Projects/Projects.js
@@ -6,8 +6,8 @@ import { urlFor, client } from '../../client';
 import './Projects.scss';
 
 const Projects = () => {
-  const [works, setWorks] = useState([]);
-  const [filterWork, setFilterWork] = useState([]);
+  const [Projects, setProjects] = useState([]);
+  const [filterproject, setFilterproject] = useState([]);
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
@@ -15,12 +15,12 @@ const Projects = () => {
     const query = '*[_type == "projects"]';
 
     client.fetch(query).then((data) => {
-      setWorks(data);
-      setFilterWork(data);
+      setProjects(data);
+      setFilterProjects(data);
     });
   }, []);
 
-  const handleWorkFilter = (item) => {
+  const handleprojectFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
@@ -28,9 +28,11 @@ const Projects = () => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
       if (item === 'All') {
-        setFilterWork(works);
+        setFilterProjects(Projects);
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)));
+        setFilterProjects(
+          Projects.filter((project) => project.tags.includes(item))
+        );
       }
     }, 500);
   };
@@ -41,12 +43,12 @@ const Projects = () => {
         My <span>Portfolio</span>
       </h2>
 
-      <div className='app-work-filter'>
+      <div className='app-project-filter'>
         {['Website', 'UI/UX', 'Small Project', 'All'].map((item, i) => (
           <div
             key={i}
-            onClick={() => handleWorkFilter(item)}
-            className={`app-work-filter-item app-flex p-text ${
+            onClick={() => handleprojectFilter(item)}
+            className={`app-project-filter-item app-flex p-text ${
               activeFilter === item ? 'item-active' : ''
             }`}
           >
@@ -58,12 +60,12 @@ const Projects = () => {
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className='app-work-portfolio'
+        className='app-project-portfolio'
       >
-        {filterWork.map((work, i) => (
-          <div className='app-work-item app-flex' key={i}>
-            <div className='app-work-img app-flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+        {filterproject.map((project, i) => (
+          <div className='app-project-item app-flex' key={i}>
+            <div className='app-project-img app-flex'>
+              <img src={urlFor(project.imgUrl)} alt={project.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
@@ -72,9 +74,9 @@ const Projects = () => {
                   ease: 'easeInOut',
                   staggerChildren: 0.7,
                 }}
-                className='app-work-hover app-flex'
+                className='app-project-hover app-flex'
               >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                <a href={project.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.8] }}
@@ -84,7 +86,7 @@ const Projects = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                <a href={project.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -97,14 +99,14 @@ const Projects = () => {
               </motion.div>
             </div>
 
-            <div className='app-work-content app-flex'>
-              <h4 className='bold-text'>{work.title}</h4>
+            <div className='app-project-content app-flex'>
+              <h4 className='bold-text'>{project.title}</h4>
               <p className='p-text' style={{ marginTop: 10 }}>
-                {work.description}
+                {project.description}
               </p>
 
-              <div className='app-work-tag app-flex'>
-                <p className='p-text'>{work.tags[0]}</p>
+              <div className='app-project-tag app-flex'>
+                <p className='p-text'>{project.tags[0]}</p>
               </div>
             </div>
           </div>
