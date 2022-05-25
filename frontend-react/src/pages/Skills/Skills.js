4b47429cea67fc3555a7700cc5c50ca249b76d5c
Chris <65775571+Birrellc@users.noreply.github.com>
2022-05-25 14:40:45 +0100

    Feat: added rough notation to skills.js and styling

--- frontend-react/src/pages/Skills/Skills.js
+++ frontend-react/src/pages/Skills/Skills.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
 
 import { urlFor, client } from '../../client';
 import './Skills.scss';
@@ -24,7 +24,11 @@ const Skills = () => {
 
   return (
     <div className='app-container skills-section '>
-      <h2 className='head-text'>Skills & Experiences</h2>
+      <RoughNotation type='highlight' show={true} color='black' padding={15}>
+        <h2 className='head-text white'>
+          Skills & <span className='yellow'>Experiences</span>
+        </h2>
+      </RoughNotation>
 
       <div className='app-skills-container'>
         <motion.div className='app-skills-list'>
@@ -41,44 +45,10 @@ const Skills = () => {
               >
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
-              <p className='p-text'>{skill.name}</p>
+              <p className='p-text white'>{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
-        <div className='app-skills-exp padding'>
-          {experiences.map((experience) => (
-            <motion.div className='app-skills-exp-item' key={experience.year}>
-              <div className='app-skills-exp-year'>
-                <p className='bold-text'>{experience.year}</p>
-              </div>
-              <motion.div className='app-skills-exp-projects'>
-                {experience.projects.map((project, i) => (
-                  <div key={i}>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className='app-skills-exp-project'
-                      data-tip
-                      data-for={project.name}
-                      key={project.name}
-                    >
-                      <h4 className='bold-text'>{project.name}</h4>
-                      <p className='p-text'>{project.company}</p>
-                    </motion.div>
-                    <ReactTooltip
-                      id={project.name}
-                      effect='solid'
-                      arrowColor='#fff'
-                      className='skills-tooltip'
-                    >
-                      {project.desc}
-                    </ReactTooltip>
-                  </div>
-                ))}
-              </motion.div>
-            </motion.div>
-          ))}
-        </div>
       </div>
     </div>
   );
